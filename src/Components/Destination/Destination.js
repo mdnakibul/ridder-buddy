ac38de09030c6cc858efc72984a56dd2d861156f
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-03-20 23:12:13 -0600

    Responsiveness fixed

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -85,7 +85,7 @@ const Destination = () => {
 
                 <div className="col-md-8">
                     <div className="map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58403.623580876025!2d90.32726106707945!3d23.8105444238919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c0c1c61277db%3A0xc7d18838730e2e59!2sMirpur%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1616262853536!5m2!1sen!2sbd" width="600" height="450" style={{border:'0px'}} allowFullScreen="" loading="lazy" title="google map"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58403.623580876025!2d90.32726106707945!3d23.8105444238919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c0c1c61277db%3A0xc7d18838730e2e59!2sMirpur%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1616262853536!5m2!1sen!2sbd" height="450" allowFullScreen="" loading="lazy" title="google map" className="map-frame"></iframe>
                     </div>
                 </div>
             </div>
